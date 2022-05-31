40508ea5e8442b4616df5a4102bc22929e076265
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-05-31 16:11:13 -0500

    price input value validated

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -20,6 +20,13 @@ export class BasicosComponent implements OnInit {
     );
   }
 
+  validPrice() {
+    return (
+      this.miFormulario?.controls.precio?.value < 0 &&
+      this.miFormulario?.controls.precio?.touched
+    );
+  }
+
   guardar() {
     console.log(this.miFormulario);
   }
