932f46ff5da346dc9073b980b8e8ee1275974d91
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-06-02 15:57:00 -0500

    add validators

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +13,9 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class BasicosComponent {
   basicosFormReactive: FormGroup = this.formBuilder.group({
-    nombre: ['RTX 4090'],
-    precio: [2000],
-    existencias: [2],
+    nombre: ['RTX 4090', [Validators.required, Validators.minLength(3)]],
+    precio: [2000, [Validators.required, Validators.min(0)]],
+    existencias: [2, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private formBuilder: FormBuilder) {}
