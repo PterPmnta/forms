fbe18affc2bf7eeeabb8f170fa1eb7d382c7e74d
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-06-16 15:58:13 -0500

    nombre and email validations using reactive forms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegistroComponent
-  ],
-  imports: [CommonModule, AuthRoutingModule],
+  declarations: [LoginComponent, RegistroComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegistroComponent implements OnInit {
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
-  constructor() { }
+  registroForm: FormGroup = this.formBuilder.group({
+    nombre: [
+      '',
+      [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
+    ],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.registroForm.reset({
+      nombre: 'Pedro Pimienta',
+    });
   }
 
+  campoNoValido(campo: string) {
+    return (
+      this.registroForm.get(campo)?.invalid &&
+      this.registroForm.get(campo)?.touched
+    );
+  }
+
+  submitFormulario() {
+    console.log(this.registroForm.value);
+  }
 }
