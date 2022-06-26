857ae78074e7c0769a78f88411b533fa6a04a80f
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-06-25 19:47:54 -0500

    Last commit section 18

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/emailValidator.service';
 import {
   FormBuilder,
   FormControl,
@@ -27,6 +28,7 @@ export class RegistroComponent implements OnInit {
           Validators.required,
           Validators.pattern(this.validatorService.emailPattern),
         ],
+        [this.emailValidator],
       ],
       username: [
         '',
@@ -42,9 +44,23 @@ export class RegistroComponent implements OnInit {
     }
   );
 
+  get emailErrorMessage(): string {
+    const errors = this.registroForm.get('email')?.errors;
+    if (errors?.required) {
+      return 'El email es obligatorio.';
+    } else if (errors?.pattern) {
+      return 'El valor ingresado no tiene formato de correo.';
+    } else if (errors?.emailTomado) {
+      return 'El correo ya esta en uso.';
+    }
+
+    return '';
+  }
+
   constructor(
     private formBuilder: FormBuilder,
-    private validatorService: ValidatorService
+    private validatorService: ValidatorService,
+    private emailValidator: EmailValidatorService
   ) {}
 
   ngOnInit(): void {
@@ -52,6 +68,8 @@ export class RegistroComponent implements OnInit {
       nombre: 'Pedro Pimienta',
       email: 'form_section@angular.dev',
       username: 'pterpmntam',
+      password: '123456',
+      password_2: '123456',
     });
   }
 
@@ -59,6 +77,27 @@ export class RegistroComponent implements OnInit {
     return (
       this.registroForm.get(campo)?.invalid &&
       this.registroForm.get(campo)?.touched
+    );
+  }
+
+  emailRequired() {
+    return (
+      this.registroForm.get('email')?.errors?.required &&
+      this.registroForm.get('email')?.touched
+    );
+  }
+
+  emailFormato() {
+    return (
+      this.registroForm.get('email')?.errors?.pattern &&
+      this.registroForm.get('email')?.touched
+    );
+  }
+
+  emailTomado() {
+    return (
+      this.registroForm.get('email')?.errors?.emailTomado &&
+      this.registroForm.get('email')?.touched
     );
   }
 
